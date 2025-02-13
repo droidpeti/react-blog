5e9aaa2e288d8757a5c6287ffc7ab711f32af6d3
Ballai Péter <ballaipeterlorant@gmail.com>
2025-02-13 11:52:29 +0100

    Added a bunch of stuff

--- src/EditPost.jsx
+++ src/EditPost.jsx
@@ -61,7 +61,7 @@ export default function EditPost() {
                             }
                         })
                     }
-
+    //
                     fetch(BASE_URL + "/posts/" + post.id, {
                         method: "PUT",
                         headers: {
